b4588806253ca3f90b2fc98fcfb86fa58cd5e30e
VarnikaChandra <123506866+VarnikaChandra@users.noreply.github.com>
2026-03-24 23:33:52 -0400

    fix App.js routing imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Resume from "./components/Resume";
 import About from "./components/About";
-
 import Projects from "./components/Projects";
 
 import "./App.css";
@@ -17,7 +16,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+
       </Routes>
     </Router>
   );
